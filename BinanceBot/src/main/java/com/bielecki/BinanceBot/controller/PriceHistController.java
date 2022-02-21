fcf47a93f28279f3782c35c583f914550f16955a
bielecki-k <bielecki-k@protonmail.com>
2022-02-21 10:58:00 +0100

    refactor

--- BinanceBot/src/main/java/com/bielecki/BinanceBot/controller/PriceHistController.java
+++
@@ -1,9 +0,0 @@
-package com.bielecki.BinanceBot.controller;
-
-import org.springframework.stereotype.Controller;
-
-//placed for rest api usage in next versions - optionally
-@Controller
-public class PriceHistController {
-
-}
